676e3a721ee3aaf1541318d2c2148625683e0b56
productdevbook <hi@productdevbook.com>
2025-12-16 23:47:02 +0300

    Add portkiller cask

---
+++ Casks/portkiller.rb
@@ -0,0 +1,19 @@
+cask "portkiller" do
+  version "2.1.0"
+  sha256 "2bed4a7801c0f172e214d260b08d5bb78cac306ee9e2b404ea8f0975819e84d3"
+
+  url "https://github.com/productdevbook/port-killer/releases/download/v#{version}/PortKiller-v#{version}-arm64.dmg"
+  name "PortKiller"
+  desc "Menu bar app to find and kill processes running on open ports"
+  homepage "https://github.com/productdevbook/port-killer"
+
+  depends_on macos: ">= :sequoia"
+  depends_on arch: :arm64
+
+  app "PortKiller.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.productdevbook.PortKiller.plist",
+    "~/Library/Caches/com.productdevbook.PortKiller",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# Homebrew Tap
+
+Homebrew tap for productdevbook apps.
+
+## Installation
+
+```bash
+brew tap productdevbook/tap
+```
+
+## Available Apps
+
+### PortKiller
+
+Menu bar app to find and kill processes running on open ports.
+
+```bash
+brew install --cask portkiller
+```
+
+**Requirements:** macOS 15.0+ (Sequoia), Apple Silicon
+
+[GitHub Repository](https://github.com/productdevbook/port-killer)
